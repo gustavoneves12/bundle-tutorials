a60e0d322375e9604fd113f66225e98e7fca6dac
gustavoneves12 <gustavo.neves12@gmail.com>
2015-09-02 15:23:59 -0300

    declare transformer in the component

--- models/orogen/tut_sensor.rb
+++ models/orogen/tut_sensor.rb
@@ -1,5 +1,12 @@
 require 'models/services/distance_bearing_sensor'
 
 TutSensor::Task.provides Tutorials::Services::DistanceBearingSensor, as: 'sensor'
-
 TutSensor::TransformerTask.provides Tutorials::Services::DistanceBearingSensor, as: 'sensor'
+
+class TutSensor::TransformerTask
+    transformer do
+        transform 'target', 'ref'
+        transform 'ref', 'world'
+        max_latency 1
+    end
+end
