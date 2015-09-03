27e6ddc96fbc5e63eecf44725e964858f21b07ff
gustavoneves12 <gustavo.neves12@gmail.com>
2015-09-03 11:04:53 -0300

    change composition namespace

--- models/compositions/rock_control.rb
+++ models/compositions/rock_control.rb
@@ -8,35 +8,36 @@ using_task_library 'tut_sensor'
 using_task_library 'rock_tutorial'
 
 module Tutorials
-    class RockControl < Syskit::Composition
+    module Compositions
+        class RockControl < Syskit::Composition
 
-        add Controldev::JoystickTask, as: 'joystick'
-        add Controldev::RawJoystickToMotion2D, as: 'converter'
-        add Rock::Services::Motion2DOpenLoopController, as: "cmd"
-        add RockTutorial::RockTutorialControl, as: 'rock'
+            add Controldev::JoystickTask, as: 'joystick'
+            add Controldev::RawJoystickToMotion2D, as: 'converter'
+            add Rock::Services::Motion2DOpenLoopController, as: "cmd"
+            add RockTutorial::RockTutorialControl, as: 'rock'
 
-        joystick_child.connect_to converter_child
-        cmd_child.connect_to rock_child
-        
-        conf 'slow',
-            cmd_child => ['default', 'slow']
-                
-        export rock_child.pose_samples_port
-        provides Rock::Services::Pose, as: 'pose'
-        
-    end
-    
-    class RockFollower < RockControl
-        overload cmd_child, TutFollower::Task
-        add Tutorials::Services::DistanceBearingSensor, as: 'sensor'
-        sensor_child.connect_to cmd_child
-        
-        specialize sensor_child => TutSensor::Task do
-            add Rock::Services::Pose, as: 'target_pose'
+            joystick_child.connect_to converter_child
+            cmd_child.connect_to rock_child
+
+            conf 'slow',
+                cmd_child => ['default', 'slow']
+
+            export rock_child.pose_samples_port
+            provides Rock::Services::Pose, as: 'pose'
             
-            target_pose_child.connect_to sensor_child.target_frame_port
-            rock_child.connect_to sensor_child.local_frame_port
         end
-        
+
+        class RockFollower < RockControl
+            overload cmd_child, TutFollower::Task
+            add Tutorials::Services::DistanceBearingSensor, as: 'sensor'
+            sensor_child.connect_to cmd_child
+
+            specialize sensor_child => TutSensor::Task do
+                add Rock::Services::Pose, as: 'target_pose'
+
+                target_pose_child.connect_to sensor_child.target_frame_port
+                rock_child.connect_to sensor_child.local_frame_port
+            end
+        end
     end
 end
